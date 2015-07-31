cf657992f4259e034b759bb26f51b07b44bcc3ef
arnolduis <arnolduis@gmail.com>
2015-07-31 13:24:27 +0200

    RED-3513

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -20,9 +20,13 @@ function supertracker() {
 		referrer = document.referrer;
 		date = new Date();
 
+        trackId = getCookie(supertrackerTrackId);
+        console.log("trackId:");
+        console.log(trackId);
+
 		if (!localStorage.supertrackerTrackId) {
 			trackId = uuid();
-			localStorage.supertrackerTrackId = trackId;
+			document.cookie = "supertrackerTrackId="+trackId+";domain=.edmdesigner.com";
 		} else {
 			trackId = localStorage.supertrackerTrackId;
 		}
@@ -203,6 +207,25 @@ function supertracker() {
 		}
 
 		return (D()+"-"+R()+"-"+UA()+"-"+S());
+	}
+
+	function setCookie(cname, cvalue, exdays, cdomain) {
+	    var d = new Date();
+	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	    var expires = "expires="+d.toUTCString();
+	    var domain = "domain=" + cdomain;
+	    document.cookie = cname + "=" + cvalue + "; " + expires + "; " + domain;
+	}
+
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i=0; i<ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') c = c.substring(1);
+	        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+	    }
+	    return "";
 	}
 
 	return {
