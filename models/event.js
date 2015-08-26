d2a6de1d929808dea06084e118937ae8971d0eab
arnolduis <arnolduis@gmail.com>
2015-08-26 10:19:20 +0200

    npm publish

--- models/event.js
+++ models/event.js
@@ -3,10 +3,10 @@ var Schema = mongoose.Schema;
 
 var eventSchema = new Schema({
 	track_id: { type: String, required: true },
-	session_id: { type: Schema.ObjectId, ref: 'Session', required: true },
+	session_id: { type: Schema.ObjectId, ref: 'Session' },
 	name: { type: String, required: true },
 	referrer: String,
-	properties: String, //ttt mixed type
+	properties: Object, //ttt mixed type
   	date: { type: Date, default: Date.now },
 	comments: String
 });
